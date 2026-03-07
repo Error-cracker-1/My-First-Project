4a41e0ec2bb29f4b9db9e2156478e4d72768a9ed
kumarnayanshukh <kumarnayanshu1@gmail.com>
2026-03-07 14:09:50 +0530

    Change In Ps1 Script

--- Powershell/Test 1.ps1
+++ Powershell/Test 1.ps1
@@ -1,22 +1,19 @@
-Write-Host "Hello, World!"
-$name = "Kumar"
-$age  = 30
-Write-Host "Name: $name, Age: $age"
-$greeting = "Hello, $name! You are $age years old."
-Write-Host $greeting
-$sum = 5 + 10
-Write-Host "The sum of 5 and 10 is: $sum"
-$numbers = @(1, 2, 3, 4, 5)
-Write-Host "Numbers: $($numbers -join ', ')"
-$today = Get-Date
-Write-Host "Today's date is: $today"
-$randomNumber = Get-Random -Minimum 1 -Maximum 100
-Write-Host "Random number between 1 and 100: $randomNumber"
-$greetingFunction = {
-    param($name)
-    return "Hello, $name!"
+Write-Host "Hi $env:USERNAME! Welcome to PowerShell scripting."
+Write-Host "Your Pc Name is $env:COMPUTERNAME and your OS is $env:OS.Version:"
+$source = Read-Host "Enter source folder"
+$dest = Read-Host "Enter backup folder"
+
+Copy-Item $source $dest -Recurse
+Write-Host "Backup completed!"
+$number = Get-Random -Minimum 1 -Maximum 10
+$guess = Read-Host "Guess a number between 1 and 10"
+
+if ($guess -eq $number) {
+    Write-Host "Correct!"
 }
-$result = & $greetingFunction -name "Kumar"
-Write-Host $result  
+else {
+    Write-Host "Wrong. The number was $number"
+}
+
 
 Read-Host "Press Enter to exit"
